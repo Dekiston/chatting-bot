965da5032d2ccff30b5dc7c6b5255db6f21ad925
Dekiston <Dekiston@yandex.ru>
2021-10-20 15:05:51 +0300

    part

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const { VK } = require('vk-io');
+const Az = require('az');
+const { VK, MessageContext } = require('vk-io');
 const  { HearManager } = require('@vk-io/hear');
 const fs = require("fs");
 const vk = new VK({ token: '0f181d9eefc80b8695f008aef4a5cde8fac5e29667d051cfcec62e0c5fb21d2c04cc1ea01ab91c38cecc5'});
@@ -45,52 +46,70 @@ bot.hear (/^mc....|mc...$/, msg => {
     msg.send ('Процент изменен.');
 }); //изменение процента
 
+
 bot.hear (/./, msg => {  //msg.text сообщение пользователя Searchfile(msg.chatId)
     let Id = msg.chatId;
     
     StatFile(Id);
-    
-    
+  
     if (chance(Id) > getRandom(100)) {
 
-        switch(getRandom(6)) {
-            case 0:  console.log (0);
+        switch(getRandom(12)) {
+          
+            case 1:
+            console.log (0);
             msg.send (answer(Id)[getRandom(answer(Id).length)]);
               break;
           
-            case 1: console.log (1);
+            case 2:
+            case 3: console.log (1);
             let message = split(Id)[getRandom(answer(Id).length)];
             let index = getRandom(150);
             while (message.length < index) { message = message + "  " + answer(Id)[getRandom(answer(Id).length)];}
             msg.send (Upperone(message.toLowerCase()));
               break;
 
-            case 2: console.log (2);
+            case 4: console.log (2);
             let message2 = (answer(Id)[getRandom(answer(Id).length)] + ' ' + answer(Id)[getRandom(answer(Id).length)]);
             msg.send (Upperone(message2.toLowerCase()));
               break;
             
-            case 3: console.log (3);
+            case 5:
+            case 6: console.log (3);
             let message3 = (answer(Id)[getRandom(answer(Id).length)] + ' ' + split(Id)[getRandom(answer(Id).length)] + " " + answer(Id)[getRandom(answer(Id).length)]);
             msg.send (Upperone(message3.toLowerCase()));
               break;
-            
-            case 4: console.log (4);
+
+            case 7:
+            case 8:
+            case 9:
+            case 10: console.log (4);
             let message4 = split(Id)[getRandom(answer(Id).length)];
             let index4 = getRandom(25);
             while (message4.length < index4) { message4 = message4 + " " + split(Id)[getRandom(answer(Id).length)];}
             msg.send (Upperone(message4.toLowerCase()));
              break;
             
-            case 5: console.log (5);
+            case 11: console.log (5);
             let message5 = split(Id)[getRandom(answer(Id).length)] + "?";
             msg.send (Upperone(message5.toLowerCase()));
-                
-            
-    };}
+            break;
+
+            case 12: console.log (6);
+            if (getRandom(10) < 8) {
+            vk.api.messages.send({peer_id: msg.peerId, sticker_id: 163, random_id: 0})} else {
+            vk.api.messages.send({peer_id: msg.peerId, sticker_id: getRandom(200), random_id: 0})};
+            break;         
+              }; } 
+
+    Az.Morph.init(function() {
+      let parses = Az.Morph(msg.text)[0].tag;
+      console.log(parses.POST, parses.GNdr); })
 
     if (answer(Id).indexOf(Upperone(msg.text)) < 0) { fs.appendFileSync(file(Id), '|' + Upperone(msg.text)); } //запись сообщения в txt файл
-    });
+    })
+
+    bot.onFallback((context) => {context.send ("Ты что высрал, пес?")})// Обработчик, если команда не существует
 
 
 console.log('Бот запущен!');
