67fd15d1fe8a8622ad9d828dd45677b4dae99a99
Dekiston <Dekiston@yandex.ru>
2021-11-22 21:49:31 +0300

    format

--- index.js
+++ index.js
@@ -1,137 +1,169 @@
-const { VK, MessageContext } = require('vk-io');
-const  { HearManager } = require('@vk-io/hear');
-const Az = require('az');
+const { VK, MessageContext } = require("vk-io");
+const { HearManager } = require("@vk-io/hear");
+const Az = require("az");
 const fs = require("fs");
-const { get } = require('http');
-const vk = new VK({ token: '937a07a228fdf17ec8049f4943b5ffa0168147655c4dbc9fefc13d0c55527a71e82c6b71c33d52c251195'});
-const bot = new HearManager(); 
-vk.updates.on('message', bot.middleware);
+const { get } = require("http");
+const vk = new VK({
+  token:
+    "937a07a228fdf17ec8049f4943b5ffa0168147655c4dbc9fefc13d0c55527a71e82c6b71c33d52c251195",
+});
+const bot = new HearManager();
+vk.updates.on("message", bot.middleware);
 
-function fileDict(Id) {return ("dictionary" + Id + ".JSON");} //название файла
-function fileProcent(Id) {return ("info" + Id + ".txt");}  //название файла 
+function fileDict(Id) {
+  return "dictionary" + Id + ".JSON";
+} //название файла
+function fileProcent(Id) {
+  return "info" + Id + ".txt";
+} //название файла
 
-
-async function StatusFiles (Id) { //создание файлов беседы
-    fs.stat(fileProcent(Id), function(err) { if (err) { fs.writeFileSync(fileProcent(Id), 'procent: 100\nwords: 0');}});
-    fs.stat(fileDict(Id),  function(err) { if (err) { fs.writeFileSync(fileDict(Id), '{ "words": [] }');}}) //слова
-    
-  
-    
+async function StatusFiles(Id) {
+  //создание файлов беседы
+  fs.stat(fileProcent(Id), function (err) {
+    if (err) {
+      fs.writeFileSync(fileProcent(Id), "procent: 100\nwords: 0");
+    }
+  });
+  fs.stat(fileDict(Id), function (err) {
+    if (err) {
+      fs.writeFileSync(fileDict(Id), '{ "words": [] }');
+    }
+  }); //слова
 } //процент
 
+function Upperone(text) {
+  return text.charAt(0).toUpperCase() + text.slice(1);
+} //Изменение регистра первой буквы^
 
+function getRandom(max) {
+  return Math.floor(Math.random() * max);
+} //случайное число
 
-function Upperone (text) {return text.charAt(0).toUpperCase() + text.slice(1)}; //Изменение регистра первой буквы^
+function fileChoice(Id, Gender, Number) {
+  let object = fs.readFileSync(fileDict(Id), "utf8");
+  object = JSON.parse(object).words;
+  let index = object.length - 1;
+  let num = 0;
+  let OneWord = object[getRandom(index)];
+  while ((OneWord.GNdr != Gender || OneWord.NMbr != Number) && num < 10) {
+    OneWord = object[getRandom(index)];
+    num += 1;
+  }
+  console.log(OneWord);
+  return OneWord.word;
+} //чтение коллекции
 
-function getRandom (max) {return Math.floor(Math.random() * max)}; //случайное число
+function procent(Id) {
+  let procent = [];
+  procent.push(fs.readFileSync(fileProcent(Id), "utf8"));
+  return procent[0].slice(8, 12);
+} //чтение процента из файла
 
+bot.hear(/^mhelp$/, (context) => {
+  context.send(
+    "minfo - вывод информации беседы\n" + "mp - изменения процента сообщений\n"
+  );
+}); //легенда
 
-function fileChoice (Id, Gender, Number) {
-let object = fs.readFileSync(fileDict(Id), "utf8");
-object = JSON.parse(object).words;
-let index = object.length - 1;
-let lastWord = object[getRandom(index)];
-while (lastWord.GNdr != Gender && lastWord.NMbr != Number) {
-    lastWord = object[getRandom(index)];
-}
-return lastWord.word;
-}; //чтение коллекции
-
-function procent (Id) {let procent = [];
-    procent.push(fs.readFileSync(fileProcent(Id), "utf8"));
-return procent[0].slice(8,12); } //чтение процента из файла
-
-const DICT = ["NOUN", "ADJF", "ADJS", "COMP", "VERB", "INFN", "PRTF", "PRTS", "GRND", "NUMR", "ADVB", "NPRO", "PRED", "PREP", "CONJ", "PRCL", "INTJ"];
-
-bot.hear (/^mhelp$/, context => {context.send ("minfo - вывод информации беседы\n" + "mp - изменения процента сообщений\n");});   //легенда
-
-bot.hear (/^minfo$/, context => { //информация по беседе
-    context.send (fs.readFileSync(fileProcent(context.chatId), "utf8"));
+bot.hear(/^minfo$/, (context) => {
+  //информация по беседе
+  context.send(fs.readFileSync(fileProcent(context.chatId), "utf8"));
 });
 
-bot.hear (/^mclear$/, context => { //очистка файлов
-        context.send ("Не очищено.");
-     });
+bot.hear(/^mclear$/, (context) => {
+  //очистка файлов
+  context.send("Не очищено.");
+});
 
-bot.hear (/^mp....|mp...$/, context => { //изменение процента ответов
-    let procent = fs.readFileSync(fileProcent(context.chatId), "utf8");
-    let lastProcent = procent.slice(9,12);
-    procent = procent.replace (lastProcent, context.text.slice(3));
-    fs.writeFileSync(fileProcent(context.chatId), procent);
-    context.send ('Процент изменен.');
+bot.hear(/^mp....|mp...$/, (context) => {
+  //изменение процента ответов
+  let procent = fs.readFileSync(fileProcent(context.chatId), "utf8");
+  let lastProcent = procent.slice(9, 12);
+  procent = procent.replace(lastProcent, context.text.slice(3));
+  fs.writeFileSync(fileProcent(context.chatId), procent);
+  context.send("Процент изменен.");
 }); //изменение процента
 
+bot.hear(/./, async (context) => {
+  //любое сообщение
+  let Id = context.chatId; //Id чата
 
-bot.hear (/./, async context => {  //любое сообщение
-    let Id = context.chatId; //Id чата
-    
-    let message = context.text.split(" "); //деление сообщения на слова
+  let message = context.text.split(" "); //деление сообщения на слова
 
-    for (let word of message) {
-        if (/[,.!?;:()]/.test(word[word.length-1])) {word = word.slice(0,-1);} //проверка на знаки препинания в конце слова
-        if (/[,.!?;:()]/.test(word)) {continue;} //пропуск одиночных знаков препинания
+  for (let word of message) {
+    if (/[,.!?;:()]/.test(word[word.length - 1])) {
+      word = word.slice(0, -1);
+    } //проверка на знаки препинания в конце слова
+    if (/[,.!?;:()]/.test(word)) {
+      continue;
+    } //пропуск одиночных знаков препинания
 
-        Az.Morph.init(async function() {let part = Az.Morph(word)[0].tag; //морфологический разбор слова
-            await StatusFiles(Id); //проверка на существование файлов беседы
-            
+    Az.Morph.init(async function () {
+      try {
+        let part = Az.Morph(word)[0].tag; //морфологический разбор слова
+        await StatusFiles(Id); //проверка на существование файлов беседы
 
-            let object = fs.readFileSync(fileDict(Id));
-                object = JSON.parse(object); //обьект  - 1 -
+        let object = fs.readFileSync(fileDict(Id));
+        object = JSON.parse(object); //обьект  - 1 -
 
-                const existingWord = object.words.filter((obj) => obj.word === word); //проверка одинаковых слов
-                if (existingWord.length > 0) { return; } 
-
-            let property = {
-                POST: part.POST,
-                word: word, //слово
-                GNdr: part.GNdr, //род
-                NMbr: part.NMbr //ед. множ. число
-            }
-            
-                object.words.push(property); //добавление данных - 2 -
-            let json = JSON.stringify(object); //обратно в JSON
-                fs.writeFileSync(fileDict(Id), json); 
-        });
-    }
-
-    if (procent(Id) > getRandom(100)) { 
-
-const Genders = ["masc", "femn", "neut"];
-let Gender = Genders[getRandom(3)];
-const Numbers = ["plur", "sing"];
-let Number = Numbers[getRandom(2)];
-
- let message;
-
- switch(getRandom(1)) {
-
-            case 0: console.log (0);
-            message = fileChoice(Id, Gender, Number);
-            for (let i = 0; i < (getRandom(10) + 2); i++) {message = message + ' ' + fileChoice(Id, Gender, Number);};    
-            message = Upperone(message.toLowerCase());
-            context.send (message);
-              break;
-
+        const existingWord = object.words.filter((obj) => obj.word === word); //проверка одинаковых слов
+        if (existingWord.length > 0) {
+          return;
         }
+        let property = {
+          POST: part.POST,
+          word: word, //слово
+          GNdr: part.GNdr, //род
+          NMbr: part.NMbr, //ед. множ. число
+          СAse: part.CAse,
+        };
 
-}
+        object.words.push(property); //добавление данных - 2 -
+        let json = JSON.stringify(object); //обратно в JSON
+        fs.writeFileSync(fileDict(Id), json);
+      } catch {}
+    });
+  }
 
-})
+  if (procent(Id) > getRandom(100)) {
+    const Genders = ["masc", "femn", "neut"];
+    const Numbers = ["plur", "sing"];
+
+    let message;
+
+    switch (getRandom(1)) {
+      case 0:
+        console.log(0);
+        message = fileChoice(Id, Genders[getRandom(3)], Numbers[getRandom(2)]);
+        for (let i = 0; i < getRandom(8) + 1; i++) {
+          message = message + " " + fileChoice(Id, Genders[getRandom(3)]);
+        }
+        message = Upperone(message.toLowerCase());
+        context.send(message);
+        break;
+    }
+  }
+});
 
 bot.onFallback((context) => {
-    switch(getRandom(5)) {
+  switch (getRandom(5)) {
+    case 0:
+      console.log("2.0");
+      context.send("АХАХАХАХА");
+      break;
 
-        case 0: console.log ('2.0');
-                context.send ("АХАХАХАХА");
-                break;
+    case 1:
+      console.log("2.1");
+      vk.api.messages.send({
+        peer_id: context.peerId,
+        sticker_id: 163,
+        random_id: 0,
+      });
+      break;
+  }
+}); // Обработчик, если команда не существует
 
-        case 1: console.log ('2.1');
-                vk.api.messages.send({peer_id: context.peerId, sticker_id: 163, random_id: 0});
-                break;        
-    
-    }})// Обработчик, если команда не существует
-
-console.log('Бот запущен!');
+console.log("Бот запущен!");
 vk.updates.start().catch(console.error);
 
 //NOUN	имя существительное	хомяк
